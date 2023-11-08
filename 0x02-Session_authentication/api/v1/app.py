ecd83ebf4fd20686f430136bf6fe5c659f255e6c
ThatAgricBoy <samueljohn3999@gmail.com>
2023-11-08 18:04:40 +0100

    Fix: pycodestyle

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,8 +7,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth  # Import the Auth class
-from api.v1.auth.session_auth import SessionAuth  # Import the SessionAuth class
+from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,14 +17,14 @@ auth = None
 
 AUTH_TYPE = os.getenv("AUTH_TYPE")
 
-# Determine the authentication mechanism based on the environment variable AUTH_TYPE
+
 if AUTH_TYPE == "auth":
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif AUTH_TYPE == "session_auth":
-    auth = SessionAuth()  # Use SessionAuth for session-based authentication
+    auth = SessionAuth()
 
 
 @app.before_request
