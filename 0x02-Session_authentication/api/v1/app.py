5895c56afbead9d797c29d467756fa849d2b100c
ThatAgricBoy <samueljohn3999@gmail.com>
2023-11-08 18:26:22 +0100

    auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,14 +38,16 @@ def before_request():
     if auth is None:
         pass
     else:
+        setattr(request, "current_user", auth.current_user(request))
         excluded_list = ['/api/v1/status/',
-                         '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                         '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                         '/api/v1/auth_session/login/']
 
         if auth.require_auth(request.path, excluded_list):
-            if auth.authorization_header(request) is None:
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
                 abort(401, description="Unauthorized")
-            request.current_user = auth.current_user(request)
-            if request.current_user is None:
+            if auth.current_user(request) is None:
                 abort(403, description='Forbidden')
 
 
