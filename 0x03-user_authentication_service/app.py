0f91f5e177a62e4f7154b1649ea99bf73ced9cf6
ThatAgricBoy <samueljohn3999@gmail.com>
2023-11-14 14:20:52 +0100

    logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,5 +57,21 @@ def login() -> str:
         return response
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> None:
+    """_summary_
+
+    Returns:
+        None: _description_
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
