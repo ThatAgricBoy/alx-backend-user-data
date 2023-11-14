4faa3ff6fac0404fbe285235c6b798031ed92cc6
ThatAgricBoy <samueljohn3999@gmail.com>
2023-11-14 14:25:27 +0100

    profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -68,15 +68,17 @@ def logout() -> str:
     AUTH.destroy_session(user.id)
     return redirect('/')
 
+
 @app.route('/profile', methods=['GET'])
 def profile() -> str:
     """_summary_
     """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if not user:
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
         abort(403)
-    return jsonify({"email": user.email}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
