1925e11982e2edc77c5c02f414189dcd9dcd085b
ThatAgricBoy <samueljohn3999@gmail.com>
2023-11-14 13:46:30 +0100

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,7 +35,7 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/login', methods=['POST'], strict_slashes=False)
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """_summary_
 
@@ -44,13 +44,17 @@ def login() -> str:
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    valid = AUTH.valid_login(email, password)
-    if not valid:
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
         abort(401)
-    session_id = AUTH.create_session(email)
-    response = jsonify({"email": email, "message": "logged in"})
-    response.set_cookie("session_id", session_id)
-    return response
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
 
 
 if __name__ == "__main__":
